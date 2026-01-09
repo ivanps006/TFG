67337605a711c064669aa3cff560b620ab9ef3ac
Ivan <a24pasaiv@iesgrancapitan.org>
2026-01-09 13:33:02 +0100

    Inicio Sesion

---
+++ src/main/java/org/example/proyecto_tfg/Launcher.java
@@ -0,0 +1,9 @@
+package org.example.proyecto_tfg;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
