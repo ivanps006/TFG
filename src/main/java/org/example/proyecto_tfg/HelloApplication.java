ee79d32b05b56459b9b836aaa04a3fcf8c6d9a27
Ivan <a24pasaiv@iesgrancapitan.org>
2026-01-09 13:33:36 +0100

    Inicio Sesion

--- src/main/java/org/example/proyecto_tfg/HelloApplication.java
+++ src/main/java/org/example/proyecto_tfg/HelloApplication.java
@@ -6,13 +6,21 @@ import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.URL;
 
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 500, 500);
         stage.setTitle("Hello!");
+
+        // Ruta correcta dentro del classpath: /css/style.css
+        URL cssUrl = getClass().getResource("/css/style.css");
+        if (cssUrl != null) {
+            scene.getStylesheets().add(cssUrl.toExternalForm());
+        }
+
         stage.setScene(scene);
         stage.show();
     }
--- src/main/java/org/example/proyecto_tfg/HelloController.java
+++ src/main/java/org/example/proyecto_tfg/HelloController.java
@@ -1,14 +1,64 @@
 package org.example.proyecto_tfg;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private TextField txtUsuario;
 
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private PasswordField txtContrasenia;
+
+    @FXML
+    private Label ErrorLogin;
+
+
+    public void validarCredenciales(){
+        if (txtUsuario.getText().equals("admin") && txtContrasenia.getText().equals("admin1234")){
+           ErrorLogin.setText("Acceso concedido. ¡Bienvenido!");
+           ErrorLogin.setStyle("-fx-text-fill: green;");
+        } else {
+            ErrorLogin.setText("Credenciales incorrectas. Inténtalo de nuevo.");
+            ErrorLogin.setStyle("-fx-text-fill: red;");
+        }
+    }
+
+    @FXML
+    private void onRegisterButtonClick(ActionEvent event) {
+        try {
+            //Cargamos la nueva ventana
+            FXMLLoader loader = new FXMLLoader(HelloApplication.class.getResource("register-view.fxml"));
+            Parent root = loader.load();
+            Stage stage = new Stage();
+            //Le añadimos este titulo a la ventana
+            stage.setTitle("Registrarse");
+            //bloquea la ventana padre
+            stage.initModality(Modality.APPLICATION_MODAL);
+            stage.setScene(new Scene(root, 400, 300));
+            stage.show();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void onClose(ActionEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 }
